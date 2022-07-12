18c6c59c7604aaaf8c030d8d434865e0b32773ca
NHazelJ <najib.mosquera@hotmail.com>
2022-07-12 18:07:49 -0400

    == vs === and != vs !==

--- Section 16 JavaScript Decision Making/Comparison Operators.js
+++ Section 16 JavaScript Decision Making/Comparison Operators.js
@@ -11,10 +11,34 @@ let age = 31
 
 age >= 18 // return true
 
+// characters have unicodes a number value so strings can be use for comparison operators
 
 
+// == vs ===
+1 == 1 // true
+
+1 == '1' // true
+
+null == undefined // true
+
+1 == 2 // false
+
+'a' == 'b' // false
+
+0 == false // true
+
+1 === 1 // true
+
+1 === '1' // false
+ 0 === false // false
 
 
+ 1 != 2 // true
 
+ 1 != '2' // true
+ 1 != '1' // false
 
+ 1 == '1' // true
+ 1 !== '1' // true
 
+ // always use === when comparing things
