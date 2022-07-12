d4900c93e302303a0d7d1f81f2e47d7e1e9120e1
NHazelJ <najib.mosquera@hotmail.com>
2022-07-12 10:58:14 -0400

    Working with Comparison Operators

---
+++ Section 16 JavaScript Decision Making/Comparison Operators.js
@@ -0,0 +1,20 @@
+1 < 3 // return true
+1 > 3 // return false
+
+-1 > 20 // return false
+-1 > -1 // return false
+
+-1 <= -1 // return true
+-1 <= 10 // return true
+
+let age = 31
+
+age >= 18 // return true
+
+
+
+
+
+
+
+
