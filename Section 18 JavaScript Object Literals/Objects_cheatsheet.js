af9dca98b9ac5098821f855c12d49b371ed72b4c
NHazelJ <najib.mosquera@hotmail.com>
2022-07-28 16:44:56 -0400

    Working with ForLoops

--- Section 18 JavaScript Object Literals/Objects_cheatsheet.js
+++ Section 18 JavaScript Object Literals/Objects_cheatsheet.js
@@ -14,3 +14,29 @@ dog["age"]; //7
 //updating values
 dog.breed = "mutt";
 dog["age"] = 8;
+
+//dynamic with a variable
+const years = {1999 : 'good', 2020: 'bad'}
+
+let birthYear = 2020
+
+years[birthYear]
+//the output will be bad
+
+
+const restaurant = {
+    name: 'Ichiran Ramen',
+    address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
+    city: 'Brooklyn',
+    state: 'NY',
+    zipcode: '11206',
+}
+
+//YOUR CODE GOES DOWN HERE:
+
+
+const fullAddress = `${restaurant["address"]}, ${restaurant["city"]}, ${restaurant["state"]} ${restaurant["zipcode"]}`;
+
+
+restaurant[fullAddress]
+
