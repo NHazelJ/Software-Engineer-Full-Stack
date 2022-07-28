ccf7fb1d3e2f49e8240003f965ef443284d90180
NHazelJ <najib.mosquera@hotmail.com>
2022-07-27 20:26:08 -0400

    Working with arrays and Object literals

---
+++ Section 17 JavaScript Array/array_cheatsheet.js
@@ -0,0 +1,21 @@
+// Making an array:
+// const colors = ["red", "orange", "yellow"];
+
+// Arrays are indexed like strings:
+// colors[0]; // "red"
+
+// They have a length:
+// colors.length; //3
+
+// Important array methods:
+//push(value) - adds value to the END of an array
+//pop() - removes and returns last value in array
+
+//unshift(val) - adds value to START of an array
+//shift() - removes and returns first element in an array
+
+// const gameBoard = [['X','O','X'],['O', null, 'X'],['O','O','X']]
+// gameBoard[1][1]
+
+//Coding Exercise solving the problem:
+airplaneSeats[3].splice(1,1, 'Hugo')
