272eedb70dd2fde67ef2fcc9f3e41010dda568a9
NHazelJ <najib.mosquera@hotmail.com>
2022-07-29 15:45:17 -0400

    iterating arrays and nestedLookps

--- Section 19 Repeating Stuff with Loops/iteratingArrays.js
+++ Section 19 Repeating Stuff with Loops/iteratingArrays.js
@@ -319,9 +319,18 @@ const animals = [
 ]
 
 for (let i = 0; i < animals.length; i++) {
-    console.log(i, animals[i])
+    console.log(i, animals[i]);
 }
 
 // for (let i = animals.length - 1; i >= 0; i--) {
 //     console.log(animals[i])
+// }
+
+// Practicing
+// const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; //DONT TOUCH THIS LINE!
+
+// // WRITE YOUR LOOP BELOW THIS LINE:
+
+// for(let i = 0 ; i < people.length; i++){
+//     console.log(people[i].toUpperCase());
 // }
