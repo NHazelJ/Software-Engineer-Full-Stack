3f0bad496cef22a7e55188deceb801bcb3f46d31
NHazelJ <najib.mosquera@hotmail.com>
2022-08-01 11:35:32 -0400

    ForOf

--- Section 19 Repeating Stuff with Loops/ForOf.js
+++ Section 19 Repeating Stuff with Loops/ForOf.js
@@ -4,17 +4,17 @@ const subreddits = ['cringe', 'books', 'chickens', 'funny', 'pics', 'soccer', 'g
 //     console.log(`Visit reddit.com/r/${subreddits[i]}`)
 // }
 
-for (let subreddit of subreddits) {
-    console.log(`Visit reddit.com/r/${subreddit}`)
-}
+// for (let subreddit of subreddits) {
+//     console.log(`Visit reddit.com/r/${subreddit}`)
+// }
 
 
 
-// const seatingChart = [
-//     ['Kristen', 'Erik', 'Namita'],
-//     ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
-//     ['Yuma', 'Sakura', 'Jack', 'Erika']
-// ]
+const seatingChart = [
+    ['Kristen', 'Erik', 'Namita'],
+    ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
+    ['Yuma', 'Sakura', 'Jack', 'Erika']
+]
 
 // for (let i = 0; i < seatingChart.length; i++) {
 //     const row = seatingChart[i];
@@ -23,11 +23,11 @@ for (let subreddit of subreddits) {
 //     }
 // }
 
-// for (let row of seatingChart) {
-//     for (let student of row) {
-//         console.log(student);
-//     }
-// }
+for (let row of seatingChart) {
+    for (let student of row) {
+        console.log(student);
+    }
+}
 
 // for (let char of "hello world") {
 //     console.log(char)
