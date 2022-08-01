53f2a39926d67da64c35cc6dabd764affa25a5d0
NHazelJ <najib.mosquera@hotmail.com>
2022-08-01 18:03:05 -0400

    working with functions and return

--- Section 20 New Introducing Functions/Functions_Intro_CODE.js
+++ Section 20 New Introducing Functions/Functions_Intro_CODE.js
@@ -1,20 +1,59 @@
-function singSong() {
-    console.log("DO");
-    console.log("RE");
-    console.log("MI");
-}
+// Dice roles
+// if you want to just create a function rollDie() to replace adding more dices
+// let die1 = Math.floor(Math.random() * 6) + 1;
 
-function greet(firstName, lastName) {
-    console.log(`Hey there, ${firstName} ${lastName[0]}.`)
-}
+//===================================================
 
-function repeat(str, numTimes) {
-    let result = '';
-    for (let i = 0; i < numTimes; i++) {
-        result += str;
-    }
-    console.log(result);
-}
+// function singSong() {
+//     console.log("DO");
+//     console.log("RE");
+//     console.log("MI");
+// }
+// singSong() You need this to call out the function
+//=================================================
+
+// function printHeart() {
+//     console.log('<3')
+// }
+// printHeart()
+
+//========================================================
+
+// function greet(firstName, lastName) {
+//     console.log(`Hey there, ${firstName} ${lastName[0]}.`)
+// }
+
+// Practice - Rant Exercise
+// function rant(message){
+//     aMessage = message.toUpperCase();
+//     for (let i=0; i < 3; i++){
+//         console.log(aMessage);
+//     }
+// }
+// rant("I hate beets")
+
+
+// function repeat(str, numTimes) {
+//     let result = '';
+//     for (let i = 0; i < numTimes; i++) {
+//         result += str;
+//     }
+//     console.log(result);
+// }
+
+//========================================================
+
+//Exercise Multiple Args Exercise - isSnakeEyes
+// function isSnakeEyes(num1, num2){
+//     if (num1 === 1 && num2 === 1)
+//         console.log('Snake Eyes!');
+//     else 
+//         console.log("Not Snake Eyes!");
+// }
+
+// isSnakeEyes(1,1);
+
+//========================================================
 
 function add(x, y) {
     if (typeof x !== 'number' || typeof y !== 'number') {
