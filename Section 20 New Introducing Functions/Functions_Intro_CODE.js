f1c0553b42cc7d1e8d58bc03e66910d5881b020d
NHazelJ <najib.mosquera@hotmail.com>
2022-08-01 21:37:03 -0400

    Working with Functions - exercises

--- Section 20 New Introducing Functions/Functions_Intro_CODE.js
+++ Section 20 New Introducing Functions/Functions_Intro_CODE.js
@@ -33,17 +33,18 @@
 // rant("I hate beets")
 
 
-// function repeat(str, numTimes) {
-//     let result = '';
-//     for (let i = 0; i < numTimes; i++) {
-//         result += str;
-//     }
-//     console.log(result);
-// }
+function repeat(str, numTimes) {
+    let result = '';
+    for (let i = 0; i < numTimes; i++) {
+        result += str;
+    }
+    console.log(result);
+}
 
 //========================================================
 
 //Exercise Multiple Args Exercise - isSnakeEyes
+
 // function isSnakeEyes(num1, num2){
 //     if (num1 === 1 && num2 === 1)
 //         console.log('Snake Eyes!');
@@ -55,12 +56,12 @@
 
 //========================================================
 
-function add(x, y) {
-    if (typeof x !== 'number' || typeof y !== 'number') {
-        return false;
-    }
-    return x + y;
-}
+// function add(x, y) {
+//     if (typeof x !== 'number' || typeof y !== 'number') {
+//         return false;
+//     }
+//     return x + y;
+// }
 
 // singSong()
 // singSong()
@@ -73,4 +74,87 @@ function add(x, y) {
 // singSong()
 // singSong()
 // singSong()
+
+//==============================================
+// Return Value Practice
+// function multiply(num1, num2){
+//     return num1*num2;
+// }
+
+// multiply(2,3);
+// multiply(9,9);
+// multiply(5,4);
+
+//=================================================
+// isShortsWeather Function Exercise
+
+function isShortsWeather(temperature) {
+    if (temperature < 75) {
+        return false;
+    } else if (temperature >= 75) {
+        return true;
+    }
+}
+
+//=====================================================
+
+// Last Element Exercise
+
+function lastElement (LEArray) {
+    if (LEArray && LEArray.length>0) {
+        return LEArray[LEArray.length - 1];
+    } return null;
+}
+
+//====================================================
+
+Capitalize Exercise
+
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+  
+console.log(capitalize('foo')); // Foo
+
+
+//=====================================================
+
+//Sum Array Exercise
+
+function sumArray(array) {
+    let total = 0;
+        for (let value of array) {
+        total += value;
+    }
+        return total;
+  }
+
+sumArray([1,2,3]) //6
+sumArray([2,2,2,2]) // 8
+sumArray([50,50,1]) // 101
+
+//======================================================
+
+// Days of the Week Exercise
+
+//creating a object
+const days = {
+    1:"Monday",
+    2:"Tuesday",
+    3:"Wednesday",
+    4:"Thursday",
+    5:"Friday",
+    6:"Saturday",
+    7:"Sunday"
+ }
+
+const dayNum = Object.values(days);
+
+function returnDay(day) {
+    if(day < 1 || day > 7) {
+        return null;
+    } else {
+        return dayNum[day - 1];
+    }
+}
 
