b65ad13c0f299fbef7a19b9123af060b5947cbf0
NHazelJ <najib.mosquera@hotmail.com>
2022-08-03 13:25:28 -0400

    Working with Expression and Higher order

---
+++ Section 21 Leveling Up Our Functions/FunctionExpressions.js
@@ -0,0 +1,16 @@
+// function add(x, y) {
+//     return x + y;
+// }
+
+const add = function (x, y) {
+    return x + y;
+}
+
+
+// Function Expression Exercise
+
+const square = function (num1) {
+    return num1*num1 ;
+    };
+square (4);
+square (3);
--- Section 21 Leveling Up Our Functions/Scope.js
+++ Section 21 Leveling Up Our Functions/Scope.js
@@ -1,20 +1,20 @@
 // ==============
 // FUNCTION SCOPE
 // ==============
-let totalEggs = 0;
-function collectEggs() {
-    totalEggs = 6;
-}
-console.log(totalEggs);
-collectEggs();
-console.log(totalEggs);
+// let totalEggs = 0;
+// function collectEggs() {
+//     totalEggs = 6;  
+// }
+// console.log(totalEggs);
+// collectEggs();
+// console.log(totalEggs);
 
-const bird = 'Scarlet Macaw';
-function birdWatch() {
-    const bird = 'Great Blue Heron';
-    console.log(bird);
-}
-birdWatch()
+// const bird = 'Scarlet Macaw';
+// function birdWatch() {
+//     const bird = 'Great Blue Heron';
+//     console.log(bird);
+// }
+// birdWatch()
 
 // ==============
 // BLOCK SCOPE
