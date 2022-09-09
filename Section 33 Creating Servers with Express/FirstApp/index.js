c1e2d6311843710a3db9d47874e47f25e875dda0
NHazelJ <najib.mosquera@hotmail.com>
2022-09-08 21:15:34 -0400

    config express for EJS

--- Section 33 Creating Servers with Express/FirstApp/index.js
+++ Section 33 Creating Servers with Express/FirstApp/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the home page!')
+    res.send('Welcome to the home page!!!')
 })
 
 app.get('/r/:subreddit', (req, res) => {
